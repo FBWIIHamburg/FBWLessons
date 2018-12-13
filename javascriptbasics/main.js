e8d8c310555f6f52fe47fc78f8b818d22ce74b9a
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2018-12-13 16:17:43 +0100

    grid responsive

---
+++ javascriptbasics/main.js
@@ -0,0 +1,33 @@
+window.onload=function(){
+ //document.getElementById("result").innerHTML="<h1>FBW</h1>"; //this line will render html and show the result
+//document.getElementById("result").innerText="<h1>FBW</h1>";   this line will show the text as it is even it is an html code
+
+
+
+//Decalring variables
+
+//Declare and show a variable
+//let x = 6 ;
+//document.getElementById("result").innerText=x;
+
+//operations
+
+let x = 6 ;
+let z = 0 ;
+
+let y = x + z ;
+let f = y == x;
+let b = x + z - y;
+
+console.log(b);
+
+
+// console.log(y);
+// console.log(x);
+// console.log(z);
+// console.log(y,x,z);
+// console.log(y+x+z);
+// console.log(y+" "+x+" "+z);
+// console.log(f);
+
+}
