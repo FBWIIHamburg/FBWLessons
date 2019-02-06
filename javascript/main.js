bc27104d28da2ec867539472bf3b39e84c1608ec
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-02-06 16:07:09 +0100

    javascript

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+
+
+
+
+let val1=4;
+{
+    console.log("over flow hidden = 5");
+    let someVar = 12;
+    console.log(someVar);
+    var variable1 = 5;
+    let val1=3;
+    console.log(val1);
+    
+}
+
+    
+
+console.log(variable1);
+console.log(val1);
---
+++ javascript/main2.js
@@ -0,0 +1,36 @@
+
+let num1 , num2 , num3;
+ num1 =3 ;
+ num2 = 4;
+ num3 = num2 / (2 * num1)  ;
+console.log(num3);
+console.log(++num1);
+console.log(5 ** 3);
+
+
+// --num1 equale num1=num1 -1;
+
+let text1 = "Hello";
+let text2 = "World";
+console.log(text1+" "+text2);
+
+let foo = 5 ;
+//foo += 2;
+foo = foo + 2 ;
+console.log(foo);
+
+
+console.log("5" == "5");
+
+
+console.clear();
+
+let nB = 23 ;
+let nM = 32 ;
+let nH = 10;
+let nK = 5;
+let nR = 2;
+console.log (nR >= nH / nK || nM < nB );
+
+
+
