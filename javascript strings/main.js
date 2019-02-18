71267f1ce25f9e03ce05d3899fa8ef52b122b454
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-02-18 11:01:22 +0100

    javascript strings

---
+++ javascript strings/main.js
@@ -0,0 +1,11 @@
+let name = "Ahmad Osman";
+//console.log(name.indexOf("O"));
+console.log(name);
+console.log(name.length);
+
+let someText="what does \"javascript\" mean?";
+console.log(someText.toString());
+
+let strictVar = new String("what does \"javascript\" mean?");
+console.log(strictVar);
+console.log(strictVar.lastIndexOf("blabla"));
