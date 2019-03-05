3faeac61a8be2bba8c6b95f042159a478679d8bf
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-03-05 11:34:37 +0100

    javascript encrypter gui2

--- javascript encrypter/main.js
+++ javascript encrypter/main.js
@@ -19,5 +19,40 @@ function decrypter(code,key){
 
 return storage;
 }
-let encryptedText="52000,50500,54000,54000,55500,16000,16000,36500,16000,48500,54500,16000,32500,52000,54500,48500,50000";
-console.log(decrypter(encryptedText,500));
+// let encryptedText="52000,50500,54000,54000,55500,16000,16000,36500,16000,48500,54500,16000,32500,52000,54500,48500,50000";
+// console.log(decrypter(encryptedText,500));
+
+function encryptClick(){
+    let inputText = document.getElementById("inputText").value; 
+    let key = document.getElementById("keyinput").value;
+    
+    if(!isNaN(key) && key.trim()!=""){
+        document.getElementById("inputcode").value=encrypter(inputText,key);
+    }
+    else{
+        alert("unvalid key number");
+    }
+}
+function decryptClick(){
+    let codeText = document.getElementById("inputcode").value; 
+    let key = document.getElementById("keyinput").value;
+    
+    if(!isNaN(key) && key.trim()!=""){
+        document.getElementById("inputText").value=decrypter(codeText,key);
+    }
+    else{
+        alert("unvalid key number");
+    }
+}
+
+function checkNumber(e){
+    
+    if (e.which < 48 || e.which > 57 )
+    {
+        
+            e.preventDefault();
+        
+        
+    }
+    
+}
