131e9e832406f6b679f47b9dc713c862b4082c01
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-04-03 09:17:34 +0200

    jquery animation

--- jquery Events/main3.js
+++ jquery Events/main3.js
@@ -1,19 +1,38 @@
 $(document).ready(function () {
+    var status=true;
     $("#button1").click(function () { 
-
-        let animation={
+        let animation;
+        if(status){
+         animation={
             left:"15rem",
             top:"50rem",
             opacity:"0.2",
             width:"50rem",
             height:"50rem",
             backgroundColor: "blue"
-            
         }
-        $(".someDiv").animate(animation,1500);
-
+        status=false;
+    }else{
+        animation={
+            left:"0",
+            top:($("#button1").height()+2)+"px",
+            opacity:"1",
+            width:"10rem",
+            height:"10rem",
+            backgroundColor: "red"
+        }
+        status=true;
+    }
+        $(".someDiv").animate(animation,1500,function(){hi("hallo");}).slideUp(6000).slideDown(3000);
         
     });
-    
-    
+
+
+    $("#button2").click(function () { 
+        $(".someDiv").stop();
+
+    });
 });
+function hi(param) {
+    alert(param); 
+ }
