78388b15e458e7a4b8de72772f1cb0289133f3a8
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-05-16 09:53:37 +0200

    restaurant mongodb connect

--- NodeJs/restaurant/app.js
+++ NodeJs/restaurant/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/src/views'));
 app.set('view engine', 'ejs');
 const mainRouter = require('./src/routes/mainRouter');
 app.use('/', mainRouter);
+const adminRoutes = require('./src/routes/adminRoutes');
+app.use('/admin',adminRoutes );
 // app.get('/', (req, res)=>{
 //     res.end("hello");
 //     });//
