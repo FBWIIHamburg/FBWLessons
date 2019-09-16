365aed3c49da7cf8bc98918ff888d1f1f076bca9
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-09-16 11:26:59 +0200

    class sql

--- 10- SQL/3- SQL and classes/app.js
+++ 10- SQL/3- SQL and classes/app.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const {Customer} = require('./controllers/classes');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -8,6 +9,8 @@ const sql = require('./controllers/sql');
 const app = express();
 const port = process.env.PORT ;
 
+let newCustomer = new Customer(1,'dci','Osman','Ahmad','123456', 'some address 1 ', '', 'Hamburg', 'Hamburg', '220099', 'Germany',null,5000, 'lion', '123456');
+
 app.get('/testconnection', (req, res) => {
     sql.connect().then(()=>{
         res.send('connected');
@@ -15,103 +18,11 @@ app.get('/testconnection', (req, res) => {
         res.send(error); 
     })
 });
-app.get('/customers', (req, res) => {
-    //get data orderd using cusomername either DESC or ASC 
-    // sql.sqlQuery('select customerName from customers order by customerName DESC').then(result=>{
-        sql.sqlQuery('select customerName from customers ').then(result=>{
-            let somArr = [];
-        let content = "";
-        result.forEach(element => {
-            somArr.push(element.customerName);
-        });
-        somArr.sort();
-        somArr.reverse();
-        somArr.forEach(element => {
-             content +=element+ '<br>';
-        });
-        res.send(content);
-    }).catch(error=>{
-        res.send(error);
-    });
-})
-;
-// show customers with name begin with A
-app.get('/customers1', (req, res) => {
-    let content = "";
-    sql.sqlQuery("select customerName from customers WHERE customerName LIKE 'A%'").then(result=>{
-        result.forEach(element => {
-            content +=element.customerName+ '<br>';
-       });
-       res.send(content);
-    }).catch(error=>{
-        res.send(error);
-    })
-});
-app.get('/customers1', (req, res) => {
-    let content = "";
-    sql.sqlQuery("select customerName from customers WHERE customerName LIKE 'A%'").then(result=>{
-        result.forEach(element => {
-            content +=element.customerName+ '<br>';
-       });
-       res.send(content);
-    }).catch(error=>{
-        res.send(error);
-    })
-});
-
-// show customer full name
-app.get('/customers2', (req, res) => {
-    let content = "";
-    sql.sqlQuery("select concat (contactFirstName , ' ' , contactLastName) as CustomerName  from customers ").then(result=>{
-        result.forEach(element => {
-            content +=element.CustomerName+ '<br>';
-       });
-       res.send(content);
-    }).catch(error=>{
-        res.send(error);
-    })
+app.get('/customer', (req, res) => {
+    res.json(Customer.getCustomerById(114));
 });
 
 
-// show customer full name from USA
-app.get('/customers3', (req, res) => {
-    let content = "";
-    sql.sqlQuery("select concat (contactFirstName , ' ' , contactLastName) as CustomerName  from customers where country like 'USA' ").then(result=>{
-        result.forEach(element => {
-            content +=element.CustomerName+ '<br>';
-       });
-       res.send(content);
-    }).catch(error=>{
-        res.send(error);
-    })
-});
-
-
-// show orders number which have been purchased in 2003 february
-app.get('/customers4', (req, res) => {
-    let content = "";
-    sql.sqlQuery("select orderNumber from orders where orderDate between '2003-02-01' and '2003-02-28'").then(result=>{
-        result.forEach(element => {
-            content +=element.orderNumber+ '<br>';
-       });
-       res.send(content);
-    }).catch(error=>{
-        res.send(error);
-    })
-});
-
-// show sum payments from USA in spicfice month 
-app.get('/customers5', (req, res) => {
-    sql.sqlQuery("select sum(payments.amount) as total from payments"+
-     " inner join customers on customers.customerNumber = "+
-     " payments.customerNumber where customers.country like 'USA' "+
-     " and payments.paymentDate between '2003-01-01' and '2003-01-30' ").then(result=>{
-        
-       res.send(result);
-    }).catch(error=>{
-        res.send(error);
-    })
-});
 app.listen(port, () => {
     console.log(`App listening on port ${port}!`);
 });
---
+++ 10- SQL/3- SQL and classes/controllers/classes.js
@@ -0,0 +1,59 @@
+const {sqlQuery} = require('./sql');
+
+class Customer{
+    constructor(id, companyname, lname,  fname, phone, address1, address2, city, state, postalcode, country, employee, creditLimit, username, password){
+this.Id = id;
+this.companyName = companyname;
+this.Lname = lname; 
+this.Fname = fname;
+this.phone = phone;
+this.address1 = address1;
+this.address2 = address2;
+this.city = city;
+this.state = state;
+this.postalCode = postalcode;
+this.country = country;
+this.employee = employee;
+this.creditLimit = creditLimit;
+this.userName = username;
+this.password = password;
+    }
+
+    //static method
+    static getCustomerById(id){
+        const queryPromise = new Promise((resolve, reject)={
+            sqlQuery(`select * from customers where customerNumber = ${id} `).then(customer=>{
+            
+                if(customer.length == 0){
+                    return null;
+                }else{
+                    let data = customer[0];
+                    return new Customer(data.customerNumber,
+                        data.customerName,
+                        data.contactLastName,
+                        data.contactFirstName,
+                        data.phone,
+                        data.addressLine1,
+                        data.addressLine2,
+                        data.city,
+                        data.state,
+                        data.postalCode,
+                        data.country,
+                        data.salesRepEmployeeNumber,
+                        data.creditLimit,
+                        data.username,
+                        data.password
+                        );
+                }
+            }).catch(error=>{
+    
+            });
+        });
+        
+
+    }
+}
+
+module.exports={
+    Customer
+}
